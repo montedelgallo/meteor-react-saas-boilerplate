ebcf37bb96ab3a4b33f885ebb075830a5699128e
Giovanni Panasiti <giova.panasiti@gmail.com>
2023-10-29 13:44:05 +0100

    add users

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import { Meteor } from 'meteor/meteor';
+import React, { useState, Fragment } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { LoginForm } from './LoginForm';
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
-);
+export const App = () => {
+  const user = useTracker(() => Meteor.user());
+  const logout = () => Meteor.logout();
+
+
+  return (
+    <div className="main">
+      {user ? (
+        <Fragment>
+          <h1>Ciao</h1>
+          <div className="user" onClick={logout}>
+            {user.username} 🚪
+          </div>
+        </Fragment>
+      ) : (
+        <LoginForm />
+      )}
+    </div>
+  )
+};
--- server/main.js
+++ server/main.js
@@ -1,38 +1,63 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
 import express from 'express'
 import { WebApp } from 'meteor/webapp'
+import { LinksCollection } from '/imports/api/links';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+const SEED_USERNAME = 'admin';
+const SEED_PASSWORD = 'password';
 
 async function insertLink({ title, url }) {
   await LinksCollection.insertAsync({ title, url, createdAt: new Date() });
 }
 
+const insertTask = taskText => TasksCollection.insert({ text: taskText });
+
+
 Meteor.startup(async () => {
   // If the Links collection is empty, add some data.
-  if (await LinksCollection.find().countAsync() === 0) {
-    await insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app',
-    });
+  // if (await LinksCollection.find().countAsync() === 0) {
+  //   await insertLink({
+  //     title: 'Do the Tutorial',
+  //     url: 'https://www.meteor.com/tutorials/react/creating-an-app',
+  //   });
 
-    await insertLink({
-      title: 'Follow the Guide',
-      url: 'https://guide.meteor.com',
-    });
+  //   await insertLink({
+  //     title: 'Follow the Guide',
+  //     url: 'https://guide.meteor.com',
+  //   });
 
-    await insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com',
-    });
+  //   await insertLink({
+  //     title: 'Read the Docs',
+  //     url: 'https://docs.meteor.com',
+  //   });
 
-    await insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com',
+  //   await insertLink({
+  //     title: 'Discussions',
+  //     url: 'https://forums.meteor.com',
+  //   });
+  // }
+
+  // if (TasksCollection.find().count() === 0) {
+  //   [
+  //     'First Task',
+  //     'Second Task',
+  //     'Third Task',
+  //     'Fourth Task',
+  //     'Fifth Task',
+  //     'Sixth Task',
+  //     'Seventh Task'
+  //   ].forEach(insertTask)
+  // }
+
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
     });
   }
 
   const app = express()
-
   app.get('/hello', (req, res) => {
     res.status(200).send('Hello World!')
   })
